dc6c431f28c081a573c11d3a454efaa213c6c077
Берковский Артем <berkovskiymzk@gmail.com>
2019-09-17 10:19:57 +0300

    Добавляем цикл подсчета слов в указанной строке. Цикл ориентируется на пробелы. Отдельно расположенные символы, считаются как слова. Можно добавить исключения, что бы не считать символы за слова, но в задании этого не было :-)

--- task 2/word_count.py
+++ task 2/word_count.py
@@ -7,3 +7,11 @@ word = 0
 lines = iin_file.readlines()[0]#Подсчет слов в первой строке
 print(lines)
 space = 'out'
+for i in lines:
+    if i != ' ' and space == 'out':
+        word +=1
+        space = 'in'
+    elif i == ' ':
+        space = 'out'
+
+print('Количество слов в строке: ', word)
