b6eae14cd1fff49d701db95eabe98650d64deaea
Берковский Артем <berkovskiymzk@gmail.com>
2019-09-17 10:05:37 +0300

    Определяем переменные: word - переменная, для определения начала счетчика слов lines - переменная, получающая список, содержащий все строки. Т.к. у нас указан '[0]', то lines вернет только первую строку space - это флаг, необходим для определения нахождения "вне слова" или "в слове"

--- task 2/word_count.py
+++ task 2/word_count.py
@@ -3,3 +3,7 @@ import os
 os.path.abspath(__file__)
 
 iin_file = open("strings.txt", "r", encoding="utf8")
+word = 0
+lines = iin_file.readlines()[0]#Подсчет слов в первой строке
+print(lines)
+space = 'out'
